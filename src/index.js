c28a6fef5cad52ccac70276e83b237168f823c9d
Nathan Petrangelo <nathan@petrangelo.org>
2020-09-16 18:04:09 -0400

    Added NameForm

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NameForm from './NameForm';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NameForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
